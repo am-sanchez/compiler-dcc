a32fbfe65e5e151f573ddf83cd1931249a566a9f
Alex <asanchez1103@gmail.com>
2024-04-28 15:00:18 -0600

    Test script working moreso as expected
    * TODO: Fix failing cases. Ensure copied cstrings are null terminated
    
    refs: #3

--- testscript.sh
+++ testscript.sh
@@ -2,18 +2,21 @@
 
 cmake . && make
 testOutFileName="testOutput.txt"
+failedTests=
 for file in samples/*.frag; do
     compFileName=$(basename -- "${file}")
     compFileName="samples/${compFileName%.*}.out"
     echo "${compFileName}"
-    $(./dcc < "${compFileName}" > "${testOutFileName}")
+    $(./dcc < "${file}" > "${testOutFileName}")
 
-    if cmp -s "${compFileName}" "${testOutFileName}"; then
+    if cmp "${compFileName}" "${testOutFileName}"; then
         printf 'The file "%s" is the same as "%s"\n' "${compFileName}" "${testOutFileName}"
         echo PASS::"${file}"
     else
         printf 'The file "%s" is different from "%s"\n' "${compFileName}" "${testOutFileName}"
         echo FAIL::"${file}"
+        failedTests="${failedTests} ${file}"
     fi
     $(rm "${testOutFileName}")
 done
+echo "Failed Tests: ${failedTests}"
