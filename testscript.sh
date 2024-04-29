0544031308855f77bfc547f3fc19f8fefab4eeb6
Alex <asanchez1103@gmail.com>
2024-04-28 20:48:54 -0600

    Aesthetic output updates to test script
    * Test will return 1 if failures are detected
    
    refs: #3

--- testscript.sh
+++ testscript.sh
@@ -3,20 +3,49 @@
 cmake . && make
 testOutFileName="testOutput.txt"
 failedTests=
+
+echo
+echo "=== dcc SCANNER TEST ==="
+echo
+echo "Comparing output of ./dcc samples/*.frag against expected output within samples/*.out..."
+echo
 for file in samples/*.frag; do
     compFileName=$(basename -- "${file}")
     compFileName="samples/${compFileName%.*}.out"
-    echo "${compFileName}"
+
     $(./dcc < "${file}" &> "${testOutFileName}")
 
-    if cmp "${compFileName}" "${testOutFileName}"; then
-        printf 'The file "%s" is the same as "%s"\n' "${compFileName}" "${testOutFileName}"
+    if cmp -s "${compFileName}" "${testOutFileName}"; then
         echo PASS::"${file}"
     else
-        printf 'The file "%s" is different from "%s"\n' "${compFileName}" "${testOutFileName}"
+        echo
         echo FAIL::"${file}"
+        printf 'The file "%s" is different from scanner output of ./dcc %s\n\n' "${compFileName}" "${file}"
+        echo "===== SCANNER OUTPUT =====":
+        cat "${testOutFileName}"
+        echo "===== END SCANNER OUTPUT =====":
+        echo
+        echo "===== EXPECTED OUTPUT =====":
+        cat "${compFileName}"
+        echo "===== END EXPECTED OUTPUT =====":
+        echo
         failedTests="${failedTests} ${file}"
     fi
     $(rm "${testOutFileName}")
 done
-echo "Failed Tests: ${failedTests}"
+echo
+if [[ "${failedTests}" != "" ]]; then
+    printf '=========== TEST FAILURES ============\nFailed Tests:\n'
+    for testName in ${failedTests}; do
+        echo "  ${testName}"
+    done
+    printf '======================================'
+else
+    echo "All dcc Scanner Tests PASS!"
+fi
+echo
+echo "=== END dcc SCANNER TEST ==="
+
+if [[ "${failedTests}" != "" ]]; then
+    exit 1;
+fi
